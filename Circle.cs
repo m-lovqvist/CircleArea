65269086e1f8d43e8e7bf8388319fde0ad2367e5
malin <malin@DESKTOP-64QT8T3>
2023-10-15 14:28:21 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    internal class Circle
+    {
+        private float _pi = 3.141f;
+        private int _Radius;
+
+        public float Pi
+        {
+            get { return _pi; }
+            set { _pi = 3.141f; }
+        }
+
+        public int Radius
+        {
+            get { return _Radius; }
+            set { _Radius = value; }
+        }
+
+        public Circle(int Radius)
+        {
+            _Radius = Radius;
+        }
+
+        /// <summary>
+        /// Räknar ut arean på en cirkel
+        /// </summary>
+        public void GetArea()
+        {
+            float i = _Radius * _Radius * _pi;
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CircleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Circle circle2= new Circle(6);
+
+            circle.GetArea();
+            circle2.GetArea();
+        }
+    }
+}
